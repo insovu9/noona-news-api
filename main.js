a37fa5f16456b862ba317a30d16e04350598fde8
insovu <insovu9@gmail.com>
2024-02-21 00:19:41 +0900

    3rd commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_KEY = `e9ccff088ea44691aa102045cfab76e3`;
 const getLatestNews = async ()=>{
-    const url = new URL (`https://noona-news-api.netlify.app/`);
+    const url = new URL (`https://noona-news-api.netlify.app/top-headlines`);
     const response = await fetch(url)
     const data = await response.json()
     let news = data.articles
